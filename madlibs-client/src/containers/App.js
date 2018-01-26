a2bfd95921b34bcae56fe8d6a1bceb91206667b0
Joanna <jmgsese@gmail.com>
2018-01-26 17:02:01 -0600

    Repair CORS issue where and No Access-Control-Allow-Origin header problem

--- madlibs-client/src/containers/App.js
+++ madlibs-client/src/containers/App.js
@@ -11,8 +11,21 @@ class App extends Component {
     }
   }
 
+
+
   componentDidMount(){
-    fetch('https://localhost:3000/api/madlibs')
+
+    const headers = new Headers({
+      // 'Accept': 'application/json',
+      // 'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    })
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+    fetch('http://localhost:3000/api/madlibs', {
+      headers: headers,
+      // mode: 'no-cors'
+    })
       .then(response => response.json())
       .then(madlibs => this.setState({ madlibs }))
   }
