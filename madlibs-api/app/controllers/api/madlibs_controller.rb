6a59494b32045ab8dc6dbabd692bf5021ce1455d
Joanna <jmgsese@gmail.com>
2018-01-26 14:09:58 -0600

    Add CRUD methods to madlibs controller

--- madlibs-api/app/controllers/api/madlibs_controller.rb
+++ madlibs-api/app/controllers/api/madlibs_controller.rb
@@ -1,9 +1,46 @@
 class Api::MadlibsController < ApplicationController
+  before_action :set_madlib, only: [:show, :update, :destroy]
+
   def index
     render json: Madlib.all
   end
 
-  def show
-    render json: Madlib.find_by_id(params[:id])
+  def create
+    madlib = Madlib.new(madlib_params)
+    if madlib.save
+      render json: madlib
+    else
+      render json: { message: madlib.errors }, status: 400
+    end
   end
+
+  def show
+    render json: @madlib
+  end
+
+  def update
+    if @madlib.update(madlib_params)
+      render json: @madlib
+    else
+      render json: { message: madlib.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @madlib.destroy
+      render json: { message: "In the trash" }, status: 204
+    else
+      render json: { message: "Unable to destroy" }, status: 400
+    end
+  end
+
+  private
+
+    def set_madlib
+      @madlib = Madlib.find_by_id(params[:id])
+    end
+
+    def madlib_params
+      params.require(:madlib).permit(:noun, :verb, :adj)
+    end
 end
