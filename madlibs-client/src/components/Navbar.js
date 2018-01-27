6b5744b7209946dc70c62bb7e964fe660fe0a941
Joanna <jmgsese@gmail.com>
2018-01-27 11:16:53 -0600

    Create Navbar component

--- madlibs-client/src/components/Navbar.js
+++ madlibs-client/src/components/Navbar.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
+
+const NavBar = () =>
+  <div>
+    <Navbar>
+      <Nav>
+        <IndexLinkContainer to='/' >
+          <NavItem>Home</NavItem>
+        </IndexLinkContainer>
+        <LinkContainer to='/madlibs'>
+          <NavItem>Madlibs</NavItem>
+        </LinkContainer>
+      </Nav>
+    </Navbar>
+  </div>
+
+export default NavBar;
