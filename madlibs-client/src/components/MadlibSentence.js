85109ba83be4b8051a80694fb0bfc088edbb9bf7
Joanna <jmgsese@gmail.com>
2018-01-28 20:03:55 -0600

    Consolidate madlib return into one container

--- madlibs-client/src/components/MadlibSentence.js
+++ madlibs-client/src/components/MadlibSentence.js
@@ -1,9 +1,25 @@
 //components
+// DON'T NEED ANYMORE. DELETE WHEN SURE.
 
 import React from 'react';
 
+let phrases = [
+  `The ADJ NOUN enjoys when I VERB.`,
+  `Everyone would VERB a ADJ NOUN.`
+];
+
 const MadlibSentence = ({ madlib }) => (
-  <p key = {madlib.id}>This is the sentence container: The <strong>{madlib.adj} {madlib.noun}</strong> always wanted to learn how to <strong>{madlib.verb}</strong>.</p>
+  <div>
+    let phrase = phrases[Math.floor(Math.random()*phrases.length)];
+    let newStr = phrase.replace(/NOUN/, {madlib.noun}).replace(/ADJ/, {madlib.adj}).replace(/VERB/, {madlib.verb})
+    return newStr;
+  </div>
 )
 
 export default MadlibSentence;
+
+
+
+// <p key={madlib.id}>
+// The <strong>{madlib.adj} {madlib.noun}</strong> always wanted to learn how to <strong>{madlib.verb}</strong>.
+// </p>
--- madlibs-client/src/containers/Madlibs.js
+++ madlibs-client/src/containers/Madlibs.js
@@ -2,18 +2,31 @@
 
 import React, { Component } from 'react';
 import { connect} from 'react-redux';
-import MadlibSentence from '../components/MadlibSentence';
 import MadlibForm from './MadlibForm'
 import { getMadlibs } from '../actions/madlibs';
 
 const Madlibs = ({madlibs}) =>
   <div>
     {madlibs.map((madlib, index, array) => {
+      
+      let phrases = [
+        `The ${madlib.adj} ${madlib.noun} enjoys when I ${madlib.verb}.`,
+        `Everyone would ${madlib.verb} a ${madlib.adj} ${madlib.noun}.`
+      ];
+
       if (array.length-1 === index) {
-        return <MadlibSentence key={madlib.id} madlib={madlib} />
+        if (madlib.noun && madlib.adj && madlib.verb){
+          const phrase = phrases[Math.floor(Math.random()*phrases.length)];
+          return phrase;
+        } else {
+          return "Hey, you forgot something!"
+        }
+        // return <MadlibSentence key={madlib.id} madlib={madlib} />
       }
     })}
   </div>
+
+
 
 // class Madlibs extends Component {
 //
