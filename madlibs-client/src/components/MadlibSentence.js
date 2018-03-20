d908e69002f44692a2738a0c47c5fe18729a812b
Joanna <jmgsese@gmail.com>
2018-03-20 16:46:33 -0500

    Edit phrases

--- madlibs-client/src/components/MadlibSentence.js
+++ madlibs-client/src/components/MadlibSentence.js
@@ -47,11 +47,11 @@ console.log(this.state.phrase.props.children)
     const {madlib} = this.props
     let phrases =
       [
-        <div key={madlib.id}>The <strong>{madlib.adj} {madlib.noun}</strong> enjoys when I <strong>{madlib.verb}</strong>.</div>,
-        <div key={madlib.id}>Everyone would <strong>{madlib.verb}</strong> a <strong>{madlib.adj} {madlib.noun}</strong>.</div>,
+        <div key={madlib.id}>The <strong>{madlib.noun}</strong> enjoys when I <strong>{madlib.verb}</strong>, but only if I am <strong>{madlib.adj}</strong>.</div>,
+        <div key={madlib.id}>Everyone would <strong>{madlib.verb}</strong> a <strong>{madlib.noun}</strong> on a <strong>{madlib.adj}</strong> day.</div>,
         <div key={madlib.id}>Sometimes I wish <strong>{madlib.noun}</strong> would be <strong>{madlib.adj}</strong> when it is time to <strong>{madlib.verb}</strong>.</div>,
         <div key={madlib.id}>Heavy is the <strong>{madlib.noun}</strong> that needs to <strong>{madlib.verb}</strong> during <strong>{madlib.adj}</strong> hour.</div>,
-        <div key={madlib.id}>The <strong>{madlib.adj} {madlib.noun}</strong> always wanted to learn how to <strong>{madlib.verb}</strong>.</div>
+        <div key={madlib.id}>A <strong>{madlib.adj}</strong> person is a <strong>{madlib.noun}</strong> when it is time to <strong>{madlib.verb}</strong>.</div>
       ]
     this.setState({phrase: phrases[Math.floor(Math.random()*phrases.length)] })
   }
